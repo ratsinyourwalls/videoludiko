b3395086b04ce8703666393cb7d86e3c9a288cef
sofnyaa <84627643+Sofnya@users.noreply.github.com>
2025-01-24 19:16:20 +0100

    Simple player

---
+++ player.gd
@@ -0,0 +1,24 @@
+extends Area2D
+
+@export var speed = 100
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var dir = Vector2.ZERO
+	if Input.is_action_pressed("UP"):
+		dir.y -= 1
+	if Input.is_action_pressed("DOWN"):
+		dir.y += 1
+	if Input.is_action_pressed("LEFT"):
+		dir.x -= 1
+	if Input.is_action_pressed("RIGHT"):
+		dir.x += 1
+	position += dir.normalized()*delta*speed
+	var windowSize = DisplayServer.window_get_size()
+	position.x = clamp(position.x, 0, windowSize.x)
+	position.y = clamp(position.y, 0, windowSize.y)
