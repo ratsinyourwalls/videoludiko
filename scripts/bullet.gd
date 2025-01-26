cfb5230b6c4ac1511278bea38253cee339479f76
sofnyaa <84627643+Sofnya@users.noreply.github.com>
2025-01-26 19:01:45 +0100

    Add world-collisions to bullets

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -29,3 +29,8 @@ func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 
 func _on_area_entered(_area:Area2D) -> void:
 	queue_free()
+
+
+func _on_body_entered(_body:Node2D) -> void:
+	queue_free()
+
